6e3d30d7b5d93b8557f388f9350a96def372b098
Luz Carolina Hernandez <109165262+LCarolina811@users.noreply.github.com>
2025-06-04 20:36:24 -0500

    TODO: Obtener puerto del ambiente

--- app.py
+++ app.py
@@ -13,6 +13,7 @@ app = create_app()
 
 if __name__ == "__main__":
     # TODO: Obtener puerto del ambiente o usar 5000 por defecto
+        port = int(os.environ.get("PORT", 5000))
     
     # TODO: Determinar si se debe usar modo debug
     
